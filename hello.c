2dc97f44b63a2bbf73553a518d932c55a504acac
Praveenajw <praveenajw@gmail.com>
2025-02-18 14:41:57 +0530

    update

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Praveena");
+    printf("COE");
+    return 0;
+}
---
+++ scanf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter number a and b with spaces: ");
+    scanf("%d %d",&a,&b);
+    printf("The addition of %d and %d is: %d",a,b,a+b);
+    return 0;
+}
